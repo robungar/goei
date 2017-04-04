b3dea6396d307434b649d274b308aa900774344a
Rob Ungar <robungar@gmail.com>
2017-04-04 19:35:36 -0400

    added theme and conversion for single tbody

--- routes/convert.js
+++ routes/convert.js
@@ -15,51 +15,55 @@ router.post('/', function(req, res, next){
 	var text = req.body.text
 	
 	var list = []
-	var fields = ['businessNAME', 'person', 'phone', 'street', 'city', 'state', 'zipcode']
-	var fieldNames = ['Business', 'Person', 'Phone', 'Street', 'City', 'State', 'Zip Code']
-	var csv = json2csv({data: list, fields: fields, fieldNames: fieldNames, quotes: ''})
+	var fields = ['businessNAME', 'person', 'role', 'phone', 'street', 'city', 'state', 'zipcode']
+	var fieldNames = ['Business', 'Person', 'Role', 'Phone', 'Street', 'City', 'State', 'Zip Code']
+	
 
 	$ = cheerio.load(text)
 	$('tr').each(function(i, element){
 		var className = element.attribs.class
 		if(className != null){
 			
+			var values = []
+
 			var businessTD = element.children[4]
 			var businessDIV = businessTD.children[0]
 			var businessANCHOR = businessDIV.children[0]
-			var businessNAME = businessANCHOR.children[0]
+			var businessNAME = businessANCHOR.children[0].data //
+			
 
 			var personTD = element.children[5]
 			var personDIV = personTD.children[0]
-			var person = personDIV.children[0]
-
+			var person = personDIV.children[0].data //
+			
 			var roleTD = element.children[6]
-			//console.log('roleTD: '+roleTD.children.length)
-			// if (roleTD.children != null){
-			// 	var role = roleTD.children[0] //FIX!!!!!!!!!!!!!
-			// 	console.log('role: '+role.data)
-			// }
+			if(roleTD.children[0] != null){
+				var role = roleTD.children[0].data
+			} else {
+				role = ''
+			}
 
 			var phoneTD = element.children[7]
 			var phoneDIV = phoneTD.children[0]
 			var phoneSPAN = phoneDIV.children[0]
-			var phone = phoneSPAN.children[0]
+			var phone = phoneSPAN.children[0].data //
 
 			var streetTD = element.children[8]
-			var street = streetTD.children[0]
-
+			var street = streetTD.children[0].data //
+			
 			var cityTD = element.children[9]
-			var city = cityTD.children[0]
+			var city = cityTD.children[0].data //
 
 			var stateTD = element.children[10]
-			var state = stateTD.children[0]
+			var state = stateTD.children[0].data //
 
 			var zipcodeTD = element.children[11]
-			var zipcode = zipcodeTD.children[0]
+			var zipcode = zipcodeTD.children[0].data //
 
 			let info = {
 				"businessNAME" : businessNAME, 
-				"person" : person, 
+				"person" : person,
+				"role" : role, 
 				"phone" : phone, 
 				"street" : street, 
 				"city" : city, 
@@ -68,13 +72,12 @@ router.post('/', function(req, res, next){
 			}
 			
 			//console.log(info)
-			if(Object.keys(info).length == 7){
-				list.push(info)
-				info = {}
-				console.log(list)
-			}
+			list.push(info)
 		}
+
 	})
+	//console.log(list)
+	var csv = json2csv({data: list, fields: fields, fieldNames: fieldNames, quotes: ''})
 
 	res.setHeader('Content-disposition', 'attachment; filename=conversion.csv')
 	res.setHeader('Content-type', 'text/plain')
@@ -84,10 +87,5 @@ router.post('/', function(req, res, next){
 	return
 	res.json({message: 'thanks'})
 })
-  
-
-		
-
-
 
 module.exports = router;
